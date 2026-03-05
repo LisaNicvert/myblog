e0c1687bd235587a1a21bced6ab9a66f008ed331
Lisa Nicvert <lisa.nicvert@fondationbiodiversite.fr>
2026-03-05 20:00:20 +0100

    fixed publication page

--- content/publications.md
+++ content/publications.md
@@ -1,3 +1,5 @@
 ---
 title: "Publications"
 ---
+
+This page lists my publications to date. You can also check out my [ORCID](https://orcid.org/0009-0006-5763-0865) or [Google Scholar](https://scholar.google.com/citations?user=S-HW0doAAAAJ&hl=fr&oi=ao) profiles.
--- scripts/bib_to_json.R
+++ scripts/bib_to_json.R
@@ -1,5 +1,6 @@
 library(bib2df)
 library(jsonlite)
+library(purrr)
 library(here)
 
 # get data folder
@@ -10,14 +11,37 @@ data_folder <- here("data")
 bib <- bib2df(file.path(scripts_folder, "publications.bib"))
 
 # Clean and select fields you want
-publications <- bib[, c("TITLE", "AUTHOR", "YEAR", "JOURNAL",
-"VOLUME", "NUMBER", "PAGES", "DOI")]
+publications <- bib[, c("CATEGORY", "TITLE", "AUTHOR", "YEAR", "JOURNAL",
+"VOLUME", "NUMBER", "PAGES", "PUBLISHER", "DOI")]
 
-publications$AUTHOR <- sapply(publications$AUTHOR, function(a) paste(a, collapse = ", "))
+# Rename categories
+old_categories <- c("ARTICLE", "PHDTHESIS", "MISC")
+new_categories <- c("Published articles", "PhD thesis", "Preprints")
+names(new_categories) <- old_categories
+
+publications$CATEGORY <- unname(new_categories[publications$CATEGORY])
+publications$AUTHOR <- sapply(publications$AUTHOR, function(a) paste(gsub(",", "", a), collapse = ", "))
 publications$TITLE <- gsub("[{}]", "", publications$TITLE)
 
+# Rename columns
 colnames(publications) <- tolower(colnames(publications))
+
+# Add notes
+# notes <- c("My first ever published article!", NA, NA, NA)
+# publications$note <- notes
+
+# Add order attribute
+category_order <- c(1, 2, 3)
+names(category_order) <- c("Published articles", "Preprints", "PhD thesis")
+
+publications <- publications |> 
+  split(publications$category) |>
+  purrr::imap(function(x, name) list(
+    name = name,
+    order = unname(category_order[name]),
+    items = x[, names(x) != "category"]
+  ))
 
 # Write to Hugo data folder
 write_json(publications, file.path(data_folder, "publications.json"), 
-           pretty = TRUE, na = "null")
+           pretty = TRUE, na = "null", auto_unbox = TRUE)
